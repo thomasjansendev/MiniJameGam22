63c801b413f0274312f35314bfc56ae483344761
Morgan White <morgancjwhite@gmail.com>
2023-09-30 11:45:14 +0100

    Item spawning

--- MiniJameGam22/Assets/Scripts/EnemyPathfinding.cs
+++ MiniJameGam22/Assets/Scripts/EnemyPathfinding.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -18,88 +17,40 @@ public class EnemyPathfinding : MonoBehaviour
     private NavMeshAgent agent;
     private GameObject player;
     private List<GameObject> waypoints = new();
-    private System.Random rnd;
-    [SerializeField] private Vector2 bottomLeftCorner;
-    [SerializeField] private Vector2 topRightCorner;
+
     [SerializeField] private GameObject emptyGameObject;
-    [SerializeField] private int rndScaleFactor;
     [SerializeField] private int minimumPathLength;
     private int curWaypoint;
     [SerializeField] private float waypointSwitchMag;
     [SerializeField] private float enemyLostSightMag;
     public PathfindingTarget target = PathfindingTarget.Undefined;
     private bool delaySwitching;
+    private WorldPoints points;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        points = GameObject.FindGameObjectWithTag("GameController").GetComponent<WorldPoints>();
         agent = GetComponent<NavMeshAgent>();
         agent.updateUpAxis = false;
-        rnd = new System.Random();
         GenerateWaypoints();
     }
 
-    private float RandomX()
-    {
-        return (float)rnd.Next((int)(bottomLeftCorner.x * rndScaleFactor), (int)(topRightCorner.x * rndScaleFactor)) /
-               rndScaleFactor;
-    }
-
-    private float RandomY()
-    {
-        return rnd.Next((int)(bottomLeftCorner.y * rndScaleFactor), (int)(topRightCorner.y * rndScaleFactor)) /
-               rndScaleFactor;
-    }
-
-    private List<Vector2> GenerateRandomPath()
-    {
-        List<Vector2> path = new();
-        bool horizontal = rnd.Next(2) == 0;
-        Vector2 waypoint1, waypoint2;
-
-        if (horizontal)
-        {
-            float y = RandomY();
-            waypoint1 = new Vector2(RandomX(), y);
-            waypoint2 = new Vector2(RandomX(), y);
-        }
-        else
-        {
-            float x = RandomX();
-            waypoint1 = new Vector2(x, RandomY());
-            waypoint2 = new Vector2(x, RandomY());
-        }
-
-        path.Add(waypoint1);
-        path.Add(waypoint2);
-        return path;
-    }
-
-    private bool CollisionAtPoint(Vector2 pos)
-    {
-        NavMeshPath path = new NavMeshPath();
-        agent.CalculatePath(pos, path);
-        return path.status != NavMeshPathStatus.PathComplete;
-    }
 
     private void GenerateWaypoints()
     {
         // trial and error to make sure no collision on waypoint spawn and path length long enough
-        while (true)
+        for (int i = 0; i < 100; i++)
         {
-            List<Vector2> path = GenerateRandomPath();
-            print(path[0]);
-            print(path[1]);
+            List<Vector2> path = points.GenerateRandomPath();
 
             if ((path[0] - path[1]).magnitude < minimumPathLength)
             {
-                print("failed cause too small");
                 continue;
             }
 
-            if (CollisionAtPoint(path[0]) || CollisionAtPoint(path[1]))
+            if (points.CollisionAtPoint(path[0]) || points.CollisionAtPoint(path[1]))
             {
-                print("failed cause collision");
                 continue;
             }
 
@@ -107,6 +58,8 @@ public class EnemyPathfinding : MonoBehaviour
             waypoints.Add(Instantiate(emptyGameObject, path[0], Quaternion.identity));
             return;
         }
+
+        throw new("Not able to generate items");
     }
 
     private void FaceTarget()
