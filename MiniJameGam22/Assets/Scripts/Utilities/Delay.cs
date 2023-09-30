fb3ed4262ac218d8931e6f349ba13497618d1e04
Thomas Jansen <thomas.chris.jansen2@gmail.com>
2023-09-30 09:58:39 +0200

    cleaned up folders

---
+++ MiniJameGam22/Assets/Scripts/Utilities/Delay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public static class Delay
+{
+    public static async void Method(Action method, float delay)
+    {
+        await Task.Delay((int)delay * 1000); // expects milliseconds 
+        method();
+    }
+
+    public static async void DestroyObj(GameObject obj, float delay)
+    {
+        await Task.Delay((int)delay * 1000); // expects milliseconds 
+        Object.Destroy(obj);
+    }
+}
