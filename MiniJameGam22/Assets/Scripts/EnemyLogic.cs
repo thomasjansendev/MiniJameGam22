99e30955f43f5bf5700d0b61e1113d1ad559e8a3
Morgan White <morgancjwhite@gmail.com>
2023-09-30 12:54:35 +0100

    sends you back to beginning of store when caught

--- MiniJameGam22/Assets/Scripts/EnemyLogic.cs
+++ MiniJameGam22/Assets/Scripts/EnemyLogic.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyLogic : MonoBehaviour
 {
+    private EnemyPathfinding pathfinding;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pathfinding = GetComponent<EnemyPathfinding>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        print("caught");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerController>().MovePlayerBackToStart();
+            pathfinding.target = PathfindingTarget.Waypointing;
+            // TODO lose items? Maybe they could fly off?
+            // TODO maybe freeze enemies while you go back to start pos to look cool?
+        }
+
     }
 }
--- MiniJameGam22/Assets/Scripts/PlayerController.cs
+++ MiniJameGam22/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rb;
-
 
     [SerializeField] private float moveForce;
     private Vector2 _moveForwardVector;
     private float _moveInput; //to store forward/back input
 
     [SerializeField] private float turnRate;
+    [SerializeField] private float goBackSpeed;
     private float _turnInput;
+    private Vector3 playerStartPos;
+    private bool movingToStartPos;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        playerStartPos = GameObject.FindGameObjectWithTag("PlayerStartPos").transform.position;
+        transform.position = playerStartPos;
     }
 
     // Update is called once per frame
@@ -28,19 +30,57 @@ public class PlayerController : MonoBehaviour
     {
         Accelerate();
         Turn();
+        OptionalMoveBackToStart();
     }
+
+    private void OptionalMoveBackToStart()
+    {
+        if (!movingToStartPos)
+        {
+            return;
+        }
+
+        print("moving back");
+
+        var step = goBackSpeed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, playerStartPos, step);
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+            Quaternion.LookRotation(Vector3.forward, Vector3.up), 0.04f);
+        if ((transform.position - playerStartPos).magnitude < 0.01f)
+        {
+            movingToStartPos = false;
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+
 
     private void Accelerate()
     {
+        if (movingToStartPos)
+        {
+            return;
+        }
+
         _moveForwardVector = transform.up * moveForce;
         _rb.AddForce(_moveInput * _moveForwardVector);
     }
 
     private void Turn()
     {
-        float turnRateValue = turnRate * _turnInput; //if no input then turnRateValue = 0
+        if (movingToStartPos)
+        {
+            return;
+        }
 
+        float turnRateValue = turnRate * _turnInput; //if no input then turnRateValue = 0
         _rb.MoveRotation(_rb.rotation + turnRateValue);
+    }
+
+    public void MovePlayerBackToStart()
+    {
+        print("starting move back");
+        movingToStartPos = true;
+        GetComponent<BoxCollider2D>().enabled = false;
     }
 
     #region Input Handling
@@ -58,12 +98,12 @@ public class PlayerController : MonoBehaviour
     void OnQuit()
     {
         // def allows this to work in the editor or in regular game
-    #if UNITY_STANDALONE
-            Application.Quit();
-    #endif
-    #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-    #endif
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 
     #endregion
