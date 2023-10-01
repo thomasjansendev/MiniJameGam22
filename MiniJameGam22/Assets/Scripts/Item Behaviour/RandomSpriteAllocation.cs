a5f124220b56d1957dc70193649df96a9b49dcd2
Thomas Jansen <thomas.chris.jansen2@gmail.com>
2023-10-01 12:27:16 +0200

    Random items

---
+++ MiniJameGam22/Assets/Scripts/Item Behaviour/RandomSpriteAllocation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpriteAllocation : MonoBehaviour
+{
+
+    [SerializeField] private Sprite[] _sprites;
+    private SpriteRenderer _spriteRenderer;
+
+    private int _rngMaxRange;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rngMaxRange = _sprites.Length;
+        print(_rngMaxRange);
+        ChoseRndSprite();
+    }
+
+    private void ChoseRndSprite()
+    {
+        int rnd = Random.Range(0, _rngMaxRange); // [minInclusive..maxExclusive)
+        _spriteRenderer.sprite = _sprites[rnd];
+    }
+    
+}
